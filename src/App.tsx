92652ed48fcae311d7224d6f1747f091b5abf25d
BigTuna <bigtuna@gmail.com>
2024-02-14 11:07:25 -0600

    replace vite with bsky

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import blueskyLogo from './assets/bluesky.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://bsky.app" target="_blank">
+          <img src={blueskyLogo} className="logo" alt="Bluesky logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Coming soon</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          {count}
-        </button>
-      </div>
     </>
   )
 }
